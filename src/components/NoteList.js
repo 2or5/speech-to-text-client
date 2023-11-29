fdb401b4eb6cbf79c9232ad01002d9b024ff9f84
Nazar <nazarbokalo0@gmail.com>
2023-11-29 12:42:59 +0200

    connerct with back

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,9 +1,29 @@
-import { faList } from "@fortawesome/free-solid-svg-icons";
+import { faEdit, faList, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, {Component} from "react";
-import { Card, Table } from "react-bootstrap";
+import { Button, ButtonGroup, Card, Table } from "react-bootstrap";
+import axios from "axios";
 
 export default class NoteList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            notes : []
+        };
+    }
+
+    componentDidMount() {
+        axios.get("http://localhost:8080/notes")
+        .then(response => response.data)
+        .then((data) => {
+            this.setState({notes: data});
+        });
+
+    }
+
+
+
         render () {
             return (
                 <Card className="border border-dark bg-dark text-white">
@@ -12,15 +32,31 @@ export default class NoteList extends Component {
                         <Table bordered hover striped variant="dark">
                             <thead>
                                 <tr>
-                                    <th>#</th>
                                     <th>Name</th>
-                                    <th>Note</th>
+                                    <th>Text</th>
+                                    <th>Date</th>
+                                    <th>Ations</th>
                                 </tr>
                             </thead>
                             <tbody>
+                            {
+                            this.state.notes.length === 0 ?
                                 <tr align="center">
-                                    <td colSpan={6}>No Notes Avalible.</td>
-                                </tr>
+                                    <td colSpan={6}>Notes Avalible.</td>
+                                </tr> :
+                                this.state.notes.map((notes) => (
+                                    <tr key={notes.id}>
+                                        <td>{notes.name}</td>
+                                        <td>{notes.text}</td>
+                                        <td>{notes.date}</td>
+                                        <td><ButtonGroup>
+                                            <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}/></Button>{' '}
+                                            <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}/></Button>{' '}
+                                        </ButtonGroup>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
                             </tbody>
                         </Table>
                     </Card.Body>
