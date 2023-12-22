8516c0133c408bd13df82d151a7813e9f59c0a0d
Nazar <nazarbokalo0@gmail.com>
2023-12-22 12:07:33 +0200

    update welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,13 @@
+import { fontSize } from "@mui/system";
 import React from "react";
 
 export default function Welcome() {
   return (
     <div className="mt-4 p-5 bg-dark text-white rounded">
-      <h1>Jumbotron Example</h1>
-      <p>Lorem ipsum...</p>
-      <h1>Hello</h1>
-      <p>This is simple </p>
+      <h1 align="center">Welcome to Speech Text</h1>
+      <p align="center" style={{fontSize: "15px"}}>Here you can record your voice and view your records in text form. Also here you can edit your notes view old notes and delete not needed notes. </p>
+      <p align="center" style={{fontSize: "15px"}}>Add Note - here you can record your voice and send to create text format. Note List - here you can view your notes and do actions.</p>
+      <h5 align="right"> Good luck and have fun</h5>
     </div>
   );
 }
